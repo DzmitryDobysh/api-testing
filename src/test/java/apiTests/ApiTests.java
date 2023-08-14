501baa4492e36a1b8b8103bae8f3d3bd2a57e9a9
Dzmitry Dobysh <Dzmitry_Dobysh@epam.com>
2023-08-14 22:21:36 +0400

    adding 3 tests to fulfill homework task

---
+++ src/test/java/apiTests/ApiTests.java
@@ -0,0 +1,37 @@
+package apiTests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+
+public class ApiTests {
+    private static final String endpoint = "https://jsonplaceholder.typicode.com/users";
+
+    @Test
+    public void testStatusCode() {
+        given()
+                .when()
+                .get(endpoint)
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testResponseHeader() {
+        given()
+                .when()
+                .get(endpoint)
+                .then()
+                .header("content-type", "application/json; charset=utf-8");
+    }
+
+    @Test
+    public void testResponseBody() {
+        given()
+                .when()
+                .get(endpoint)
+                .then()
+                .body("", hasSize(10));
+    }
+}
